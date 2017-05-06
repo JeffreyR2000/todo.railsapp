9c6941099244825b133588270fe1f8d287c36fe8
Jeffrey Rusovskiy <jrusovskiy123@gmail.com>
2017-05-06 18:17:41 +0000

    omg we're saving again

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,6 +2,7 @@ class ListController < ApplicationController
     def index
     end
     def show
-      @list_name = ['Chores', 'Memes', 'Thoughts', 'People I Hate'].sample
+      @list_name = 'Chores'
+      @todo_description = ['laundry', 'Vaccum room', 'go to school']
     end
 end
